8b5c571b31d91c77e4f1124667ec9ed3f3cf63fe
luliye <aa15877153704@outlook.com>
2024-09-20 12:16:39 +0800

    格式不对齐

--- src/views/Home/CaregoryBox.vue
+++ src/views/Home/CaregoryBox.vue
@@ -1,8 +1,11 @@
 <template>
  <div>
   <div class="sortBox">
-    <div>
-     <a href="/" title="分类" class="index-all-hook"><i class="fa  fa-list"></i><span>分类</span></a>
+    <div class="index-all-hook">
+     <a href="/" title="分类" class="Icon-container">
+      <i class="fa  fa-list Icon"></i>
+      <span class="Sort-Text">分类</span>
+    </a>
      <div>
       <div class="indexSortListBox">
        <div class="coverBox"></div>
@@ -28,6 +31,19 @@
 </script>
 
 <style scoped>
+.Sort-Text,.Icon{
+  display: inline-block;
+  text-align: center;
+  /* vertical-align:middle; */
+  font-size: 24px;
+
+}
+
+.Icon{
+  position: relative;
+  padding-right: 5px;
+}
+
 .sortBox{
   display: flex;
   position: absolute;
@@ -38,7 +54,8 @@
 .indexList{
  position: absolute;
  left: 0;
- top: 1px;
+ top: 0;
+ padding: 0.8em;
  width: 300px;
  height: 400px;
  z-index: 1000;
@@ -53,16 +70,23 @@
  left: 0;
 }
 
+.Icon-container{
+  position: absolute;
+  top: 3px;
+  left: 0.8em;
+  font-weight: bold;
+  border-radius: 20px 20px 0 0;
+  text-decoration: none;
+  color: #FFF;
+}
+
 .index-all-hook{
- position: absolute;
  display: block;
  width: 300px;
  height: 50px;
  top: 0;
- color: #FFF;
- font-size: 24px;
+
  background-color: #FF8000;
- font-weight: bold;
  border-radius: 20px 20px 0 0;
  text-decoration: none;
 }
