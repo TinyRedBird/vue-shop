a17aa5c02108278ca4d584f831cbf37e637bdfb5
luliye <aa15877153704@outlook.com>
2024-09-20 12:17:13 +0800

    内容居中

--- src/views/Shop/index.vue
+++ src/views/Shop/index.vue
@@ -1,13 +1,18 @@
 
 <template>
-  <div>
+  <div class="Shop-Container">
     <ShopLayout></ShopLayout>
     <ShopDetailBox></ShopDetailBox>
   </div>
 </template>
 
 <script setup>
-
 import ShopLayout from '../Shop/ShopLayout.vue'
 import ShopDetailBox from '../Shop/ShopDetailBox.vue'
 </script>
+
+<style>
+.Shop-Container{
+  margin: 0 auto;
+}
+</style>
